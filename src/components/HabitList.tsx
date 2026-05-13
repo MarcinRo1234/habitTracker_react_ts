d1c9d2b2ea08ac878e2722506d8bac984e2046b1
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-05-13 15:46:36 +0200

    date format

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,8 +1,12 @@
+import { format } from "path";
+import Button from "./Button";
+import { eachDayOfInterval, endOfWeek, startOfWeek } from "date-fns";
+
 export default function HabitList() {
   const habits = [
-    { id: 1, name: "kokos" },
-    { id: 2, name: "lata" },
-    { id: 3, name: "stadem" },
+    { id: "", name: "kokos" },
+    { id: "", name: "lata" },
+    { id: "", name: "stadem" },
   ];
 
   if (habits.length === 0) {
@@ -16,8 +20,35 @@ export default function HabitList() {
   return (
     <div className="flex flex-col gap-3">
       {habits.map((habit) => (
-        <h1 key={habit.id}>{habit.name}</h1>
+        <HabitItem key={habit.id} habit={habit} />
       ))}
+    </div>
+  );
+}
+
+type HabitItemProps = {
+  habit: { id: string; name: string };
+};
+
+function HabitItem({ habit }: HabitItemProps) {
+  const visibleDates = eachDayOfInterval({
+    start: startOfWeek(new Date(), { weekStartsOn: 1}),
+    end: endOfWeek(new Date(), { weekStartsOn: 1})
+  }) ;
+  return (
+    <div className="rounded-xl bg-zinc-800 p-4 flex flex-col gap-3">
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex gap-3 items-center">
+          <span className="font-medium">{habit.name}</span>
+          <span className="text-sm text-amber-400">3</span>
+        </div>
+        <Button text="Delete"/>
+      </div>
+      <div className="flex gap-1.5">
+        {visibleDates.map(date => (
+          <Button key={date.toISOString()} text={(<><span className="font-medium">{ format(date, "EEE")}</span><span>{format(date, "d")}</span></>)} />
+        ))}
+      </div> 
     </div>
   );
 }
